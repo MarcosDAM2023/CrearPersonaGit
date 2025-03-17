12bcd0c320da8119e44bef5d5be04f49494fbaed
Marcos Martinez <marmarfer43@alu.edu.gva.es>
2025-03-17 18:50:37 +0100

    Mostramos en pantalla titulo Estudiantes y lineas.

--- PROYECTO2/src/paquete2/CrearPersona.java
+++ PROYECTO2/src/paquete2/CrearPersona.java
@@ -8,6 +8,10 @@ public class CrearPersona {
 		
 		Persona persona2 = new Persona("Juan", 27);
 		persona2.mostrarInformacion();
+		
+		System.out.println("----------------------------------------");
+		System.out.println("Estudiantes");
+		System.out.println("-----------");
 	}
 
 }
